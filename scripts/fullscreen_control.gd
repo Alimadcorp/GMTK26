92bed4195fd24f3611e8901a0ad00220624a9db3
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-07-26 18:00:02 +0500

    add silly button

--- scripts/fullscreen_control.gd
+++ scripts/fullscreen_control.gd
@@ -10,3 +10,7 @@ func _on_toggled(toggled_on: bool) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	OS.shell_open("https://github.com/Alimadcorp/GMTK26")
